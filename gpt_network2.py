0c46d48ff0948433dcf9ef3b4eec2051139f26e9
karimEid <karimeid93@hotmail.com>
2024-09-14 11:16:46 +0200

    Starting to implement gpt2 again

---
+++ gpt_network2.py
@@ -0,0 +1,20 @@
+"""
+Code implementing gpt2 network optimized for
+gpu training. 
+
+Implement flash training
+
+"""
+
+class MaskedMultiHeadAttention:
+    def __init__(self):
+        pass
+
+
+class GPT:
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
+    
