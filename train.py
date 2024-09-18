e6aac587409f61c021784838e68daa5d5a667eb5
karimEid <karimeid93@hotmail.com>
2024-09-18 14:53:33 +0200

    added timing to see gpu util

--- train.py
+++ train.py
@@ -1,13 +1,19 @@
 
 import torch 
+import time
 
 from prepare_data import DataLoader
 from gpt_network import GPT, GPTConfig
 
+torch.manual_seed(13424)
+if torch.cuda.is_available:
+    torch.cuda.manual_seed(13424)
 
 B, T = 4, 6
 
-train_data = DataLoader(B, T)
+train_loader = DataLoader(B, T)
+
+#torch.set_float32_matmul_precision('high')
 
 device = 'cpu'
 if torch.cuda.is_available():
@@ -18,11 +24,17 @@ model.to(device)
 
 optim = torch.optim.AdamW(model.parameters(), lr = 3e-4 )
 for i in range(50):
-    xb, yb = train_data.next_batch()
+    t0 = time.time()
+    xb, yb = train_loader.next_batch()
     xb, yb = xb.to(device), yb.to(device)
 
     logits, loss = model(xb, yb)
+    #import code; code.interact(local=locals())
     optim.zero_grad()
     loss.backward()
     optim.step()
-    print(f"step {i} with loss: {loss.item()}")
+    #torch.cuda.synchronize()
+    t1 = time.time()
+    dt = (t1-t0)*1000
+    tokens_per_sec = train_loader.B * train_loader.T / (t1-t0)
+    print(f"step {i} with loss: {loss.item():.4f}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec:.2f}")
