c626762505004a8d1d97a0c17019048191b72087
karimEid <karimeid93@hotmail.com>
2024-09-24 22:16:25 +0200

    added torch.compile

--- train_script.py
+++ train_script.py
@@ -1,14 +1,19 @@
+"""
+Initialize model, move to gpu, compile and train
+"""
+
 from gpt_network import GPT
 from config import GPTConfig
 from trainer import Trainer 
-
+import torch
 
 device='cuda'
 
 model = GPT(GPTConfig())
-
 model.to(device)
+model = torch.compile(model)
 
 trainer = Trainer(model)
-
 trainer.train_model()
+
+# Aim for 185k tokens / sec
